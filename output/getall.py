e95a176634e4468b149e741c69baecd029963285
Foldblade <foldblade@yeah.net>
2017-07-16 18:32:24 +0800

    Add files via upload

---
+++ output/getall.py
@@ -0,0 +1,21 @@
+﻿#encoding:utf-8
+import linecache
+
+months = ['11','12','1','3','4','5','6']
+all = []
+
+for month in months :
+	f = open(month+'月.txt')
+	lines = f.readlines() 
+	for line in lines :
+		print(line)
+		if line[0]!='2':
+			all.append(line)
+		
+print(all)
+
+f = open ('allhistory.txt','a+')
+
+for line in all :
+        f.write(line)
+f.close()
